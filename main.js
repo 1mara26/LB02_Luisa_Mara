f8e5d45f444259a646222246b483240ddbf31592
1mara <103266823+1mara26@users.noreply.github.com>
2022-12-23 13:47:02 +0100

    Countdown erstellt und + 1 Card

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-var slidePosition = 1;
-SlideShow(slidePosition);
-
-// forward/Back controls
-function plusSlides(n) {
-    SlideShow(slidePosition += n);
-}
-
-//  images controls
-function currentSlide(n) {
-    SlideShow(slidePosition = n);
-}
-
-function SlideShow(n) {
-    var i;
-    var slides = document.getElementsByClassName("Containers");
-    var circles = document.getElementsByClassName("dots");
-    if (n > slides.length) {slidePosition = 1}
-    if (n < 1) {slidePosition = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < circles.length; i++) {
-        circles[i].className = circles[i].className.replace(" enable", "");
-    }
-    slides[slidePosition-1].style.display = "block";
-    circles[slidePosition-1].className += " enable";
-}
-//  Countdown start
-
 (function () {
     const second = 1000,
         minute = second * 60,
@@ -64,5 +34,3 @@ function SlideShow(n) {
             //seconds
         }, 0)
 }());
-
-//  Countdown end
